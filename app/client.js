862b979a7536f429259719a76cead9eba398a494
Joe Double <joer1978@gmail.com>
2015-06-10 18:20:49 -0700

    tweaking code to add invidivual buttons for each word generator

--- app/client.js
+++ app/client.js
@@ -1,14 +1,24 @@
 $(function(){
-  $("button").on("click", function(){
+  $("#adjButton").on("click", function(){
     $.get("/adjectives", function(adjdata) {
     $("#adjectives").html(adjdata.word);
-  });
+    });
+  })
+});
+
+$(function(){
+  $("#verbButton").on("click", function(){
     $.get("/verbs", function(verbdata) {
     $("#verbs").html(verbdata.word);
-  });
+   });
+  })
+});
+
+$(function(){
+  $("#nounButton").on("click", function(){
     $.get("/nouns", function(noundata) {
     $("#nouns").html(noundata.word);
-  });
+    });
   })
 });
 
