cb56e4f3369769a3c455592595fd24135c9ea356
Joe Double <joer1978@gmail.com>
2015-06-14 20:41:51 -0700

    fixed issues that jshint and jscs found

--- app/client.js
+++ app/client.js
@@ -1,61 +1,69 @@
-$(function(){
+'use strict';
 
-  $("form").on("submit", function(e){
+$(function() {
+
+  $('form').on('submit', function(e) {
     e.preventDefault();
   });
 
-  $("#submitAdj").on("submit", function(e){
+  $('#submitAdj').on('submit', function(e) {
     e.preventDefault();
-    var adjectives = $("input[name=adjective").val();
+    var adjectives = $('input[name=adjective').val();
     var adjPost;
     if (adjectives) {
-      adjPost = {word : adjectives};
-      $.post("adjectives", adjPost, function(response){
+      adjPost = {word: adjectives};
+      $.post('adjectives', adjPost, function(response) {
         var adjectiveRes = response.msg;
-        $("#adjectiveRes").text(adjectiveRes);
+        $('#adjectiveRes').text(adjectiveRes);
       });
     }
-    $("form").trigger("reset");
+
+    $('form').trigger('reset');
   });
 
-  $("#submitVerb").on("submit", function(e){
+  $('#submitVerb').on('submit', function(e) {
     e.preventDefault();
-    var verbs = $("input[name=verb").val();
+    var verbs = $('input[name=verb').val();
     var verbPost;
     if (verbs) {
-      verbPost = {word : verbs};
-      $.post("verbs", verbPost, function(response){
+      verbPost = {word: verbs};
+      $.post('verbs', verbPost, function(response) {
         var verbRes = response.msg;
-        $("#verbRes").text(verbRes);
+        $('#verbRes').text(verbRes);
       });
     }
-    $("form").trigger("reset");
+
+    $('form').trigger('reset');
   });
 
-  $("#submitNoun").on("submit", function(e){
+  $('#submitNoun').on('submit', function(e) {
     e.preventDefault();
-    var nouns = $("input[name=noun").val();
+    var nouns = $('input[name=noun').val();
     var nounPost;
     if (nouns) {
-      nounPost = {word : nouns};
-      $.post("nouns", nounPost, function(response){
+      nounPost = {word: nouns};
+      $.post('nouns', nounPost, function(response) {
         var nounRes = response.msg;
-        $("#nounRes").text(nounRes);
+        $('#nounRes').text(nounRes);
       });
     }
-    $("form").trigger("reset");
+
+    $('form').trigger('reset');
   });
 
-  $("#bandNameButton").on("click", function(){
-    $.get("/adjectives", function(data) {
-    $("#adjective").html(data.word);
+  $('#bandNameButton').on('click', function() {
+    $.get('/adjectives', function(data) {
+      $('#adjective').html(data.word);
     });
-    $.get("/verbs", function(data) {
-    $("#verb").html(data.word);
+
+    $.get('/verbs', function(data) {
+      $('#verb').html(data.word);
     });
-    $.get("/nouns", function(data) {
-    $("#noun").html(data.word);
+
+    $.get('/nouns', function(data) {
+      $('#noun').html(data.word);
     });
+
     $('#bandName').css('textTransform', 'capitalize');
-  })
+  });
 });
--- server.js
+++ server.js
@@ -1,32 +1,34 @@
-var express = require("express");
+'use strict';
+
+var express = require('express');
 var app = express();
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
-var getRandomWord = require("./lib/getRandomWords.js");
-var Adjectives = require("./lib/adjectives.js");
+var getRandomWord = require('./lib/getRandomWords.js');
+var Adjectives = require('./lib/adjectives.js');
 var adjectives = new Adjectives();
-var Verbs = require("./lib/verbs.js");
+var Verbs = require('./lib/verbs.js');
 var verbs = new Verbs();
-var Nouns = require("./lib/nouns.js");
+var Nouns = require('./lib/nouns.js');
 var nouns = new Nouns();
 
-app.listen(port, function(){
+app.listen(port, function() {
   console.log('Server started on port ' + port);
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/app/"));
+app.use(express.static(__dirname + '/app/'));
 
-app.get("/adjectives", function(req, res){
+app.get('/adjectives', function(req, res) {
   res.json(getRandomWord(adjectives));
 });
 
-app.get("/verbs", function(req, res){
+app.get('/verbs', function(req, res) {
   res.json(getRandomWord(verbs));
 });
 
-app.get("/nouns", function(req, res){
+app.get('/nouns', function(req, res) {
   res.json(getRandomWord(nouns));
 });
 
@@ -45,17 +47,16 @@ app.post('/nouns', function(req, res) {
   res.json(word);
 });
 
-function postWord (word, wordObject){
-  if(wordObject.hasOwnProperty(word)){
-    return {msg: "We already have your word, " + word + ", in our list."};
+function postWord(word, wordObject) {
+  if (wordObject.hasOwnProperty(word)) {
+    return {msg: 'We already have your word, ' + word + ', in our list.'};
   }
+
   wordObject[word] = true;
   console.dir(wordObject);
-  return {msg: "Thanks for adding " + word + " to our word list."};
-  };
+  return {msg: 'Thanks for adding ' + word + ' to our word list.'};
+}
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/app/index.html");
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/app/index.html');
 });
-
-
